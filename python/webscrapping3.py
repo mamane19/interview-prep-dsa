715ef8b1076f648786a994e11387e809fe33c146
mamane19 <b.moussaama@alustudent.com>
2021-12-06 21:02:55 +0200

    fix: scrapping fetched the wrong details

--- python/webscrapping3.py
+++ python/webscrapping3.py
@@ -9,6 +9,16 @@ import time
 
 base_url = "https://www.amazon.com/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_pg_1?_encoding=UTF8&pg={}"
 start_time = time.time()
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/56.0.2924.76 "
+    "Safari/537.36",
+    "Upgrade-Insecure-Requests": "1",
+    "DNT": "1",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+}
 
 
 def soup_it(url):
@@ -48,6 +58,44 @@ def books_data(soup):
         books_list.append({"title": title, "price": price, "stars": stars})
     return books_list
 
+    # books_list = []
+    # stars_list = []
+    # for star in stars:
+    #     stars_list.append({"stars": star.select(".a-link-normal")[1].text})
+    # return stars
+    # for book in books:
+    #     books_list.append(
+    #         {
+    #             "title": book.select(".p13n-sc-truncate")[0].text,
+    #             "price": book.select(".p13n-sc-price")[0].text,
+    #             # we want to match every book with its stars from the stars list
+    #             "stars": [
+    #                 star["stars"]
+    #                 for star in stars_list
+    #                 if star["stars"]
+    #                 in book.select(".a-size-normal")
+    #             ],
+    #         }
+    #     )
+    # return books_list
+    # for item in stars:
+    #     books_list.append(
+    #         {
+    #             "suite1": item.select(".a-link-normal")[1].text,
+    #             # "suite2": item.select(".s-size-small"),
+    #         }
+    #     )
+    # # return books_list
+    # for book in books:
+    #     books_list.append(
+    #         {
+    #             "title": book.select(".p13n-sc-truncate")[0].text,
+    #             "price": book.select(".p13n-sc-price")[0].text,
+    #             "stars":
+    #         }
+    #     )
+    # return books_list
+
 
 def get_most_popular_books(books_list):
     """
@@ -82,8 +130,16 @@ def main():
     books_list_1 = books_data(page_1)
     books_list_2 = books_data(page_2)
 
+    # print(books_list_1)
+    # print("\n")
+    # # print(books_list_2)
+    # get the most popular books from both pages
     most_popular_books1 = get_most_popular_books(books_list_1)
     most_popular_books2 = get_most_popular_books(books_list_2)
+
+    # print(most_popular_books1)
+    # print("\n")
+    # print(most_popular_books2)
 
     # get the 10 most expensive books among the most popular books from both pages
     most_expensive_books = get_10_most_expensive_books(
@@ -97,11 +153,7 @@ def main():
     print("The 10 most expensive books among the most popular are:")
     books = []
     for book in most_expensive_books:
-        books.append(
-            "{}   -Price: {} -Reviews: {}".format(
-                book["title"], book["price"], book["stars"]
-            )
-        )
+        books.append("{} - {} - {}".format(book["title"], book["price"], book["stars"]))
     print("\n".join(books))
     print("\n")
 
