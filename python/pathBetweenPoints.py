81d2996aaa14333cd433f88397d872f75b4e00dd
mamane19 <b.moussaama@alustudent.com>
2022-01-04 16:07:48 +0200

    feat: new questions in python folder

--- python/pathBetweenPoints.py
+++ python/pathBetweenPoints.py
@@ -23,19 +23,32 @@
 
 
 def pathBetweenPoints(listOfPoints, startPoint, endPoint):
-     
+    visited = []
+    path = []
+    def find_shortest_path(current_point):
+     #    print(current_point)
+        if current_point == endPoint:
+            return path
+        visited.append(current_point)
+        for i in listOfPoints:
+            if i['name'] == current_point:
+                for j in i['connections']:
+                    if j not in visited:
+                        path.append(current_point)
+                        return find_shortest_path(j)
+        return "no path"
+    return find_shortest_path(startPoint)
+
+listOfPoints = [
+    {"name": "A", "connections": ["B", "C"]},
+    {"name": "B", "connections": ["A", "E"]},
+    {"name": "C", "connections": ["A", "D"]},
+    {"name": "D", "connections": ["C"]},
+    {"name": "E", "connections": ["B", "F"]},
+    {"name": "F", "connections": ["E"]},
+]
 
 
-# listOfPoints = [
-#     {"name": "A", "connections": ["B", "C"]},
-#     {"name": "B", "connections": ["A", "E"]},
-#     {"name": "C", "connections": ["A", "D"]},
-#     {"name": "D", "connections": ["C"]},
-#     {"name": "E", "connections": ["B", "F"]},
-#     {"name": "F", "connections": ["E"]},
-# ]
-
-
-# print(pathBetweenPoints(listOfPoints, "A", "F"))
-# print(pathBetweenPoints(listOfPoints, "D", "B"))
-# print(pathBetweenPoints(listOfPoints, "A", "D"))
+print(pathBetweenPoints(listOfPoints, "A", "F"))
+print(pathBetweenPoints(listOfPoints, "D", "B"))
+print(pathBetweenPoints(listOfPoints, "A", "D"))
