72c18d36d91b84ed80917529f5f21d0877c40682
mamane19 <b.moussaama@alustudent.com>
2022-01-04 17:40:23 +0200

    feat: new questions in python folder

---
+++ python/paintingHouses.py
@@ -0,0 +1,9 @@
+# This question is asked by Apple. 
+# You are tasked with painting a row of houses in your neighborhood such that each house is painted 
+# either red, blue, or green. The cost of painting the ith house red, blue or green, is given by costs[i][0], 
+# costs[i][1], and costs[i][2] respectively. Given that you are required to paint each house and no two adjacent 
+# houses may be the same color, return the minimum cost to paint all the houses.
+
+# Ex: Given the following costs array…
+# costs = [[1, 3, 5],[2, 4, 6],[5, 4, 3]], return 8.
+# Paint the first house red, paint the second house blue, and paint the third house green.
