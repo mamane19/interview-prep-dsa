c461f2735361c1d7e84c788f24cf28bcf46a97ae
mamane19 <b.moussaama@alustudent.com>
2022-01-04 17:36:03 +0200

    feat: new questions in python folder

--- python/stairs.py
+++ python/stairs.py
@@ -13,6 +13,14 @@
 # 1 step + 2 steps
 # 2 steps + 1 step
 
+# Ex: Given the following value of N…
+# N = 4, return 5
+# 1 step + 1 step + 1 step + 1 step
+# 1 step + 1 step + 2 steps
+# 1 step + 2 steps + 1 step
+# 2 steps + 1 step + 1 step
+# 2 steps + 2 steps
+
 
 def count_ways(n):
     if n == 1:
@@ -21,3 +29,29 @@ def count_ways(n):
         return 2
     else:
         return count_ways(n-1) + count_ways(n-2)
+
+# iterative solution
+
+
+def count_ways_iter(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        ways = [0] * (n+1)
+        ways[1] = 1
+        ways[2] = 2
+        for i in range(3, n+1):
+            ways[i] = ways[i-1] + ways[i-2]
+        return ways[n]
+
+
+print(count_ways(2))
+print(count_ways_iter(2))
+print(count_ways(3))
+print(count_ways_iter(3))
+print(count_ways(4))
+print(count_ways_iter(4))
+print(count_ways(5))
+print(count_ways_iter(5))
