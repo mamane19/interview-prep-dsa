96c9a2d8fffbe65ad4cfee9be3a7fd6ddc0b0cef
mamane19 <b.moussaama@alustudent.com>
2022-01-04 15:06:20 +0200

    feat: new questions in python folder

---
+++ python/oneGoneMissing.py
@@ -0,0 +1,28 @@
+# This question is asked by Amazon. 
+# Given an array that contains all distinct values from zero through N except one number, 
+# return the number that is missing from the array.
+
+# Ex: Given the following array nums…
+# nums = [1, 4, 2, 0], return 3.
+# 3 is the only number missing in the array between 0 and 4.
+
+# Ex: Given the following array nums…
+# nums = [6, 3, 1, 2, 0, 5], return 4.
+# 4 is the only number missing in the array between 0 and 6.
+
+
+def find_missing_number(nums):
+     # Write your code here
+     nums.sort()
+     for i in range(len(nums)):
+          if nums[i] != i:
+               return i
+     return len(nums)
+
+
+nums = [1, 4, 2, 0]
+print(find_missing_number(nums))
+
+
+nums = [6, 3, 1, 2, 0, 5]
+print(find_missing_number(nums))
