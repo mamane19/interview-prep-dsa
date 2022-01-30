433cdf44337cbdf2a4ac8dcdec6890672891507f
mamane19 <b.moussaama@alustudent.com>
2022-01-30 21:50:35 +0200

    feat(new question): tracking time and space question

--- python/trackTime.py
+++ python/trackTime.py
@@ -1,58 +1,58 @@
-# # Implemenet a basic measure of tracking the time taken by an algorithm
+# Implemenet a basic measure of tracking the time taken by an algorithm
 
 
-# import sys
-# import time
+import sys
+import time
 
 
-# def trackTime(func):
-#     def wrapper(*args, **kwargs):
-#         start = time.time()
-#         func(*args, **kwargs)
-#         end = time.time()
-#         return ("Time taken by function: {}".format(end - start))
-#     return wrapper
+def trackTime(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        return ("Time taken by function: {}".format(end - start))
+    return wrapper
 
 
-# @trackTime
-# def sumOfSquares(n):
-#     sum = 0
-#     for i in range(1, n + 1):
-#         sum += i ** 2
-#     return sum
+@trackTime
+def sumOfSquares(n):
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i ** 2
+    return sum
 
 
-# @trackTime
-# def sumOfCubes(n):
-#     sum = 0
-#     for i in range(1, n + 1):
-#         sum += i ** 3
-#     return sum
+@trackTime
+def sumOfCubes(n):
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i ** 3
+    return sum
 
 
-# if __name__ == "__main__":
-#     print(sumOfSquares(100))
-#     print(sumOfCubes(100))
+if __name__ == "__main__":
+    print(sumOfSquares(100))
+    print(sumOfCubes(100))
 
 
-# # Implement a basic measure of tracking the space used by an algorithm
+# Implement a basic measure of tracking the space used by an algorithm
 
-# def trackSpace(func):
-#     def wrapper(*args, **kwargs):
-#         sys.setrecursionlimit(10000)
-#         start = sys.getsizeof(sys.getrecursionlimit())
-#         func(*args, **kwargs)
-#         end = sys.getsizeof(sys.getrecursionlimit())
-#         return ("Space taken by function: {}".format(end - start))
-#     return wrapper
-
-
-# @trackSpace
-# def factorial(n):
-#     if n == 1:
-#         return 1
-#     return n * factorial(n - 1)
+def trackSpace(func):
+    def wrapper(*args, **kwargs):
+        sys.setrecursionlimit(10000)
+        start = sys.getsizeof(sys.getrecursionlimit())
+        func(*args, **kwargs)
+        end = sys.getsizeof(sys.getrecursionlimit())
+        return ("Space taken by function: {}".format(end - start))
+    return wrapper
 
 
-# if __name__ == "__main__":
-#     print(factorial(100))
+@trackSpace
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+if __name__ == "__main__":
+    print(factorial(100))
